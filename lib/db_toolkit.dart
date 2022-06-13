785e3d992927d383f814670ea014e62e740e02de
FMotalleb <fmotalleb@gmail.com>
2022-06-13 15:42:06 +0430

    min (pubspec)

--- lib/db_toolkit.dart
+++ lib/db_toolkit.dart
@@ -1,8 +1,8 @@
 import 'package:hive/hive.dart';
 
 abstract class DataBaseHandler {
-  static Future<Box<Map>> initCrashlytixDb() {
-    Hive.init('/home/motalleb/Documents/crashlytix_data');
+  static Future<Box<Map>> initCrashlytixDb(String path) {
+    Hive.init(path);
     return Hive.openBox<Map>('crashlytix_logs');
   }
 }
